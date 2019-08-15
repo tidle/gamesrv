8549e721c21fc97faa631e51e8e91df38c64d078
Your Name <you@example.com>
2019-08-15 16:21:20 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,135 @@
+from flask import Flask
+import flask
+from flask_socketio import SocketIO, emit
+import time
+
+app = Flask(__name__)
+app.secret_key = "BPkWbuRcyLfTp7mcwrLB3NTt"  # from random.org
+sio = SocketIO(app)
+
+chess_state = {}
+
+
+def chess_prune_rooms():
+    DELAY = 60
+    for g in list(chess_state.keys()):
+        if time.time() - chess_state[g]["last_move_s"] > DELAY:
+            del chess_state[g]
+
+
+def get_uname():
+    try:
+        return flask.session["username"]
+    except KeyError:
+        flask.session["username"] = "Anonymous"
+        return flask.session["username"]
+
+
+@app.route("/")
+def main():
+    chess_prune_rooms()
+    chess_rooms = list(chess_state.keys())
+    new = 1  # The new room's number
+    while str(new) in chess_rooms:
+        new += 1
+    s_chess_rooms = []
+    for idx, j in enumerate(chess_rooms):
+        if chess_state[j]["black"] and chess_state[j]["white"]:
+            s_chess_rooms.append(j)
+            del chess_rooms[idx]
+        elif not (chess_state[j]["black"] or chess_state[j]["white"]):
+            del chess_rooms[idx]
+    player = {}
+    for k, v in chess_state.items():
+        s = ""
+        if v["wname"] != "N/A":
+            s += v["wname"]
+            if v["bname"] != "N/A":
+                s += ' vs '
+                s += v["bname"]
+        elif v["bname"] != "N/A":
+            s += v["bname"]
+        player[k] = s
+    return flask.render_template("home.html", chess_rooms=chess_rooms, s_chess_rooms=s_chess_rooms, new=new, player=player)
+
+
+@app.route("/login")
+def login():
+    return flask.render_template('login.html')
+
+
+@app.route("/", methods=["POST"])
+def login_main():
+    flask.session["username"] = flask.request.form['name']
+    return main()
+
+
+# CHESS
+
+
+@app.route("/chess/<room>/")
+def chess_html(room):
+    return flask.render_template('chess.html', room=room, name=get_uname())
+
+
+@app.route("/chess/<room>/<path:path>")
+def chess(room, path):
+    return flask.send_from_directory("chess", path)
+
+
+@app.route("/chess/<room>/script.js")
+def chess_script(room):
+    return flask.render_template("chess.js", room=room, name=get_uname())
+
+
+@sio.on("chess move")
+def chess_move(msg):
+    emit("chess move", msg, broadcast=True)
+    chess_state[msg["room"]]["board"] = msg["fen"]
+    chess_state[msg["room"]]["last_move_s"] = time.time()
+
+
+@sio.on("chess reclock")
+def chess_reclock(room):
+    chess_state[room]["last_move_s"] = time.time()
+
+
+@sio.on("chess get")
+def chess_get(data):
+    try:
+        emit("chess get", chess_state[data["room"]])
+    except KeyError:
+        chess_state[data["room"]] = {}
+        chess_state[data["room"]]["board"] = "new"
+        chess_state[data["room"]]["white"] = False
+        chess_state[data["room"]]["black"] = False
+        chess_state[data["room"]]["bname"] = "N/A"
+        chess_state[data["room"]]["wname"] = "N/A"
+        chess_state[data["room"]]["last_move_s"] = time.time()
+        emit("chess get", chess_state[data["room"]])
+
+
+@sio.on("chess jblack")
+def chess_jblack(data):
+    emit("chess jblack")
+    emit("chess oblack", data, broadcast=True)
+    try:
+        chess_state[data["room"]]["black"] = True
+        chess_state[data["room"]]["bname"] = data["name"]
+    except KeyError:
+        pass
+
+
+@sio.on("chess jwhite")
+def chess_jwhite(data):
+    emit("chess jwhite")
+    emit("chess owhite", data, broadcast=True)
+    try:
+        chess_state[data["room"]]["white"] = True
+        chess_state[data["room"]]["wname"] = data["name"]
+    except KeyError:
+        pass
+
+
+if __name__ == "__main__":
+    sio.run(app, host="0.0.0.0")
---
+++ templates/chess.js
@@ -0,0 +1,140 @@
+const socket = io();
+
+socket.on("connect", () => {
+    socket.emit("chess get", { room: "{{ room }}" });
+    console.log("sent request");
+});
+
+window.onload = function () {
+    $("#pb").attr("disabled", true);
+    $("#pw").attr("disabled", true);
+    var game = new Chess();
+    var player = 's';
+    function updateStatus() {
+        if (game.turn() === "w") {
+            $("#whom").html("It is white's turn");
+        } else {
+            $("#whom").html("It is black's turn");
+        }
+        if (game.history().length >= 1) {
+            var move = game.history({ verbose: true });
+            console.log(move);
+            move = move[move.length - 1];
+            console.log(move);
+            $("#board").find('.square-55d63').removeClass('highlight');
+            $("#board").find('.square-' + move.from).addClass('highlight');
+            $("#board").find('.square-' + move.to).addClass('highlight');
+        }
+    }
+    function onDragStart(_, piece, _, _) {
+        if (piece[0] !== player) { return false; }
+    }
+    function onDrop(source, target) {
+        if (player !== game.turn()) { return "snapback"; }
+
+        var move = game.move({
+            from: source,
+            to: target,
+            promotion: 'q' //always promote to q
+        });
+
+        if (move === null) { return "snapback"; }
+
+        console.log("sent move", move.san);
+
+        socket.emit("chess move", {
+            room: "{{ room }}",
+            move: move.san,
+            color: move.color,
+            fen: game.fen(),
+        });
+
+    }
+    function onSnapEnd() {
+        board.position(game.fen());
+        updateStatus();
+    }
+    var config = {
+        draggable: true,
+        position: 'start',
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd
+    };
+    var board = Chessboard("board", config);
+    socket.on("chess move", (data) => {
+        console.log("got move", data.move);
+        if (
+            game.turn() == data.color &&
+            "{{ room }}" == data.room
+        ) {
+            game.move(data.move);
+            if (game.move !== null) {
+                board.position(game.fen());
+            }
+        }
+        setTimeout(function () {
+            if (game.game_over()) {
+                if (game.in_checkmate()) {
+                    alert("Checkmate!");
+                } else if (game.in_draw()) {
+                    alert("Draw!");
+                } else if (game.in_stalemate()) {
+                    alert("Stalemate!");
+                }
+            }
+        }, 100);
+        updateStatus();
+    });
+    socket.on("chess get", (data) => {
+        b = data.board;
+        console.log("got data", data);
+        if (b !== "new") {
+            board.position(b);
+            game = new Chess(b);
+        }
+        $("#pb").attr("disabled", data.black);
+        $("#pw").attr("disabled", data.white);
+        $("#pwhite").html(data.wname);
+        $("#pblack").html(data.bname);
+        $("#status").removeClass("warning").addClass("status").html("Connected to server as a spectator");
+        setInterval(function () {
+            socket.emit("chess reclock", "{{ room }}");
+        }, 50);
+        updateStatus();
+    });
+    socket.on("chess jwhite", () => {
+        player = 'w';
+        console.log("joined white");
+        board.orientation("white");
+        $("#status").html("Connected to server as white player");
+    });
+    socket.on("chess jblack", () => {
+        player = 'b';
+        console.log("joined black");
+        board.orientation("black");
+        $("#status").html("Connected to server as black player");
+    });
+    socket.on("chess owhite", (data) => {
+        if (data.room === "{{ room }}") {
+            $('#pw').attr("disabled", true);
+            $("#pwhite").html(data.name);
+            console.log("owhite");
+        }
+    });
+    socket.on("chess oblack", (data) => {
+        if (data.room === "{{ room }}") {
+            $('#pb').attr("disabled", true);
+            $("#pblack").html(data.name);
+            console.log("oblack");
+        }
+    });
+    $('#pb').on('click', function () {
+        socket.emit("chess jblack", { room: "{{ room }}", name: "{{ name }}" });
+        console.log("sent request");
+    });
+    $('#pw').on('click', function () {
+        socket.emit("chess jwhite", { room: "{{ room }}", name: "{{ name }}" });
+        console.log("sent request");
+    });
+}
