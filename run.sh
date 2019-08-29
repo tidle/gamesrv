0995e471cdf8cf51a4ed74a0bb2a1ab616f33fce
Your Name <you@example.com>
2019-08-29 16:56:03 -0700

    made run.sh better

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ then
 elif [ $t = "stop" ]
 then
     echo "Stopping server..."
-    ps aux | grep "app.py" | head -n1 | awk '{print $2}' | xargs kill
+    ps aux | grep "python3 -u app.py" | head -n1 | awk '{print $2}' | xargs kill
 elif [ $t = "restart" ]
 then
     $0 stop
