abff82deec8593530227f7392d81a66c601a0b9f
Your Name <you@example.com>
2019-08-29 16:18:43 -0700

    Added restart

--- run.sh
+++ run.sh
@@ -15,7 +15,12 @@ then
     python3 -u app.py &> log.txt &
 elif [ $t = "stop" ]
 then
+    echo "Stopping server..."
     ps aux | grep "app.py" | head -n1 | awk '{print $2}' | xargs kill
+elif [ $t = "restart" ]
+then
+    $0 stop
+    $0 start
 else
     echo "Please choose a valid option:"
     echo "start (default), or stop"
